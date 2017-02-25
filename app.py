61511034db40979f8bdff2a60b30356bd1b99004
Jordi Franco <jordi.franco@scmspain.com>
2017-02-25 13:51:08 +0100

    app will now run in port 8080

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def hello():
     return 'Hello World! I am %s' % ( container_id)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
